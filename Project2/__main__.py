f7aa8988e2453646bef366b1806bd93d76121672
Benjamin Baesu <bbaesu@gmail.com>
2020-10-26 21:25:43 -0700

    Project 2 Init

---
+++ Project2/__main__.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+# CPSC 463 | SOFTWARE TESTING
+# WRITTEN BY:
+#           Benjamin Baesu
+#           (Add Group Member Name)
+#           (Add Group Member Name)
+#           (Add Group Member Name)
+
+import menu
+
+# Capability 1: Screen that shows all rooms and their current status.
+# Capability 2: Screen showing a list of the rooms and who is staying in the room for each day for the next 7 days.
+# Capability 3: A reservation screen showing a list of all reservations currently in the system
+# Capability 4: A housekeeping screen to manage housekeeping
+# Capability 5: A guest profile screen to show guest information
+# Capability 6: Current stay screen showing a guest’s information for their current stay.
+# Capability 7: A search screen to search for guests
+# Capability 8: A daily report screen
+
+print('Welcome to Hotel Management System\n')
+
+menuChoice = menu.printMenu()
---
+++ Project2/menu.py
@@ -0,0 +1,23 @@
+# PRINT MENU FUNCTION
+def printMenu():
+    menu = ['(1) - Room Status',
+            '(2) - 7 Day Guest List',
+            '(3) - Current Reservations',
+            '(4) - Manage Housekeeping',
+            '(5) - Guest Profile',
+            '(6) - Guest Stay Information',
+            '(7) - Search Guests',
+            '(8) - Daily Report']
+
+    print("MENU:")
+    for m in menu:
+        print(m)
+
+    while True:
+        x = int(input("Enter your menu choice: "))
+        if 1 <= x <= 8:
+            break
+        else:
+            print('That is an invalid menu option. Try again.\n')
+
+    return x
